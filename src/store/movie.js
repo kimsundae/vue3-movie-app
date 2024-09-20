0761f9a5872db8fe0199f7cc91b565cd718985ce
kimgeunbae <naa159@naver.com>
2024-09-20 23:35:30 +0900

    영화 검색 코드 리팩토링

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,48 +1,73 @@
-import axios from 'axios';
-import _uniqBy from 'lodash/uniqBy';
+import axios from "axios";
+import _uniqBy from "lodash/uniqBy";
 export default {
-    // module
-    namespaced: true,
-    // data
-    state: () => ({
-        movies: [],
-        message: '',
-        loading: false,
-    }),
-    // computed
-    getters: {},
-    // methods
-    mutations: {
-        updateState(state, payload) {
-            Object.keys(payload).forEach((key) => {
-                state[key] = payload[key];
-            });
-        },
-        resetMovies(state) {
-            state.movies = [];
-        },
+  // module
+  namespaced: true,
+  // data
+  state: () => ({
+    movies: [],
+    message: "",
+    loading: false,
+  }),
+  // computed
+  getters: {},
+  // methods
+  mutations: {
+    updateState(state, payload) {
+      Object.keys(payload).forEach((key) => {
+        state[key] = payload[key];
+      });
     },
-    actions: {
-        async searchMovies({ state, commit }, payload) {
-            const { title, type, number, year } = payload;
-            const OMDB_API_KEY = '7035c60c';
-            const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`);
-            const { Search, totalResults } = res.data;
-            commit('updateState', {
-                movies: _uniqBy(Search, 'imdbID'),
-            });
-            const total = parseInt(totalResults, 10);
-            const pageLength = Math.ceil(total / 10);
-            if (pageLength > 1) {
-                for (let page = 2; page <= pageLength; page++) {
-                    if (page > number / 10) break;
-                    const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`);
-                    const { Search } = res.data;
-                    commit('updateState', {
-                        movies: [...state.movies, ..._uniqBy(Search, 'imdbID')],
-                    });
-                }
-            }
-        },
+    resetMovies(state) {
+      state.movies = [];
     },
+  },
+  actions: {
+    async searchMovies({ state, commit }, payload) {
+      try {
+        const { title, type, number, year } = payload;
+
+        const res = await _fetchMovie({ ...payload, page: 1 });
+        const { Search, totalResults } = res.data;
+        commit("updateState", {
+          movies: _uniqBy(Search, "imdbID"),
+        });
+        const total = parseInt(totalResults, 10);
+        const pageLength = Math.ceil(total / 10);
+        if (pageLength > 1) {
+          for (let page = 2; page <= pageLength; page++) {
+            if (page > number / 10) break;
+            const res = await _fetchMovie({
+              ...payload,
+              page,
+            });
+            const { Search } = res.data;
+            commit("updateState", {
+              movies: [...state.movies, ..._uniqBy(Search, "imdbID")],
+            });
+          }
+        }
+      } catch (message) {
+        commit("updateState", { movies: [], message });
+      }
+    },
+  },
 };
+
+function _fetchMovie(payload) {
+  const { title, type, year, page } = payload;
+  const OMDB_API_KEY = "7035c60c";
+  const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`;
+
+  return new Promise((resolve, reject) => {
+    axios
+      .get(url)
+      .then((res) => {
+        if (res.data.Error) reject(res.data.Error);
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(err.message);
+      });
+  });
+}
